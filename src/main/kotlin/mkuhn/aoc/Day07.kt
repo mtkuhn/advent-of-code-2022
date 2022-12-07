e8bf49fa7043277d3261a334edc23f55cd6eec09
mtkuhn <junkpilemk@protonmail.com>
2022-12-07 07:07:27 -0700

    Day 7 - improving code

--- src/main/kotlin/mkuhn/aoc/Day07.kt
+++ src/main/kotlin/mkuhn/aoc/Day07.kt
@@ -8,54 +8,22 @@ fun main() {
     println(day07part2(input))
 }
 
-fun day07part1(input: List<String>): Int {
-    val commands = input.associateCommandsToOutput()
-    val fileMap = FileNode("/", 0, true, null)
-    val dirList = mutableListOf<FileNode>(fileMap)
-    var currentNode = fileMap
-    commands.drop(1).forEach { command ->
-        when {
-            command.first == "ls" -> command.second.forEach {
-                currentNode.addFromLsResult(it).apply { if(this.isDirectory) dirList += this }
-            }
-            command.first.startsWith("cd") -> currentNode =
-                when(command.first.splitToPair(' ').second) {
-                    "/" -> fileMap
-                    ".." -> currentNode.parent!!
-                    else -> currentNode.children.first { it.name == command.first.splitToPair(' ').second }
-                }
-        }
-    }
-
-    return dirList.map { it.tallySize() }
+fun day07part1(input: List<String>): Int =
+    FileStructure().apply {
+        expandByCommands(input.associateCommandsToOutput().drop(1))
+    }.dirList.map { it.tallySize() }
         .filter { it <= 100000 }
         .sum()
-}
 
 
-fun day07part2(input: List<String>): Int {
-    val commands = input.associateCommandsToOutput()
-    val fileMap = FileNode("/", 0, true, null)
-    val dirList = mutableListOf<FileNode>(fileMap)
-    var currentNode = fileMap
-    commands.drop(1).forEach { command ->
-        when {
-            command.first == "ls" -> command.second.forEach {
-                currentNode.addFromLsResult(it).apply { if(this.isDirectory) dirList += this }
-            }
-            command.first.startsWith("cd") -> currentNode =
-                when(command.first.splitToPair(' ').second) {
-                    "/" -> fileMap
-                    ".." -> currentNode.parent!!
-                    else -> currentNode.children.first { it.name == command.first.splitToPair(' ').second }
-                }
-        }
+fun day07part2(input: List<String>): Int =
+    FileStructure().apply {
+        expandByCommands(input.associateCommandsToOutput().drop(1))
+    }.let { structure ->
+        structure.dirList.map { it.tallySize() }
+        .filter { it >= 30000000 - (70000000 - structure.root.tallySize()) }
+        .min()
     }
-
-    val totals = dirList.map { it.name to it.tallySize() }.toMap()
-    val spaceToFree = 30000000-(70000000-totals["/"]!!)
-    return totals.values.filter { it >= spaceToFree }.min()
-}
 
 fun List<String>.associateCommandsToOutput(): List<Pair<String, List<String>>> =
     this.fold(mutableListOf<Pair<String, MutableList<String>>>()) { acc, a ->
@@ -65,6 +33,26 @@ fun List<String>.associateCommandsToOutput(): List<Pair<String, List<String>>> =
     }
 
 fun String.splitToPair(separator: Char) = this.substringBefore(separator) to this.substringAfter(separator)
+
+class FileStructure(val root: FileNode = FileNode("/", 0, true, null),
+                    val dirList: MutableList<FileNode> = mutableListOf(root)) {
+    fun expandByCommands(commands: List<Pair<String, List<String>>>) {
+        var currentNode = root
+        commands.forEach { command ->
+            when {
+                command.first == "ls" -> command.second.forEach {
+                    currentNode.addFromLsResult(it).apply { if(this.isDirectory) dirList += this }
+                }
+                command.first.startsWith("cd") ->
+                    currentNode = when(command.first.splitToPair(' ').second) {
+                        "/" -> root
+                        ".." -> currentNode.parent!!
+                        else -> currentNode.children.first { it.name == command.first.splitToPair(' ').second }
+                    }
+            }
+        }
+    }
+}
 
 data class FileNode(val name: String, val size: Int, val isDirectory: Boolean,
                     val parent: FileNode?, val children: MutableList<FileNode> = mutableListOf()) {
@@ -77,5 +65,5 @@ data class FileNode(val name: String, val size: Int, val isDirectory: Boolean,
                 }
             }.apply { this@FileNode.children += this }
 
-    fun tallySize(): Int = (this.size + children.sumOf { it.tallySize() }).apply { "$name size=$this" }
+    fun tallySize(): Int = (this.size + children.sumOf { it.tallySize() }).apply { "$name size=$this" } //todo: caching?
 }
