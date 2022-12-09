09cb650b023f6dee436ac0b34c352d6022bed417
mtkuhn <junkpilemk@protonmail.com>
2022-12-09 08:00:36 -0700

    Day 9 - Part 2 and quick cleanup

--- src/main/kotlin/mkuhn/aoc/Day09.kt
+++ src/main/kotlin/mkuhn/aoc/Day09.kt
@@ -14,7 +14,7 @@ fun day09part1(input: List<String>): Int {
     val moves = input.map { it.splitToPair(' ') }
         .map { it.first.first() to it.second.toInt() }
 
-    val history = mutableListOf(Rope())
+    val history = mutableListOf(Rope(listOf(Point(0, 0), Point(0, 0))))
     moves.forEach { m ->
         repeat(m.second) {
             history += history.last().moveRopeInDirection(m.first)
@@ -24,10 +24,24 @@ fun day09part1(input: List<String>): Int {
     return history.distinctBy { it.tail }.count()
 }
 
-fun day09part2(input: List<String>): Int =
-    2
+fun day09part2(input: List<String>): Int {
+    val moves = input.map { it.splitToPair(' ') }
+        .map { it.first.first() to it.second.toInt() }
 
-data class Rope(val head: Point = Point(0, 0), val tail: Point = Point(0, 0)) {
+    val history = mutableListOf(Rope(List(10) { Point(0, 0) }))
+    moves.forEach { m ->
+        repeat(m.second) {
+            history += history.last().moveRopeInDirection(m.first)
+        }
+    }
+    return history.distinctBy { it.tail }.count()
+}
+
+data class Rope(val knots: List<Point>) {
+
+    val head = knots.first()
+    val tail = knots.last()
+
     fun moveRopeInDirection(direction: Char): Rope {
         val newHead = when(direction) {
             'U' -> Point(head.x, head.y-1)
@@ -36,25 +50,25 @@ data class Rope(val head: Point = Point(0, 0), val tail: Point = Point(0, 0)) {
             'L' -> Point(head.x-1, head.y)
             else -> error("invalid movement")
         }
-        return Rope(newHead, tail.moveToward(newHead))
+        return mutableListOf(newHead).apply {
+            knots.drop(1).forEach { k ->
+                this += k.moveToward(this.last())
+            }
+        }.let { Rope(it) }
     }
 
     fun Point.moveToward(target: Point): Point {
         var newX = this.x
         var newY = this.y
-        if(!this.isTouchingVertical(target)) {
+        if(!this.isTouching(target)) {
             if(this.y > target.y) { newY = this.y - 1 }
             if(this.y < target.y) { newY = this.y + 1 }
-        }
-        if(!this.isTouchingHorizontal(target)) {
             if(this.x > target.x) { newX = this.x - 1 }
             if(this.x < target.x) { newX = this.x + 1 }
         }
         return Point(newX, newY)
     }
     
-    fun Point.isTouchingHorizontal(target: Point) =
-        target.x in (this.x-1 .. this.x+1) && target.y in (this.y-1 .. this.y+1)
-    fun Point.isTouchingVertical(target: Point) =
+    fun Point.isTouching(target: Point) =
         target.x in (this.x-1 .. this.x+1) && target.y in (this.y-1 .. this.y+1)
 }
