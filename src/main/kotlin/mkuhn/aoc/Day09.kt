91741a5ea92fe73af67c6e31ff6ccd2ad0d4f56c
mtkuhn <junkpilemk@protonmail.com>
2022-12-09 09:22:12 -0700

    Day 9 - even more cleanup

--- src/main/kotlin/mkuhn/aoc/Day09.kt
+++ src/main/kotlin/mkuhn/aoc/Day09.kt
@@ -13,60 +13,45 @@ fun main() {
 fun day09part1(input: List<String>): Int {
     val moves = input.map { it.lineToMovement() }
     val rope = Rope(listOf(Point(0, 0), Point(0, 0)))
-    return applyMovementAndGetTailHistory(rope, moves).count()
+    return applyMovementAndGetTailPositions(rope, moves).count()
 }
 
 fun day09part2(input: List<String>): Int {
     val moves = input.map { it.lineToMovement() }
     val rope = Rope(List(10) { Point(0, 0) })
-    return applyMovementAndGetTailHistory(rope, moves).count()
+    return applyMovementAndGetTailPositions(rope, moves).count()
 }
 
 fun String.lineToMovement() = this.splitToPair(' ').let { it.first.first() to it.second.toInt() }
 
-fun applyMovementAndGetTailHistory(rope: Rope, moves: List<Pair<Char, Int>>): Set<Point> {
-    var workingRope = rope
-    val tailHistory = mutableSetOf(rope.tail)
-    moves.forEach { m ->
-        repeat(m.second) {
-            workingRope = workingRope.moveRopeInDirection(m.first)
-            tailHistory += workingRope.tail
-        }
-    }
-    return tailHistory
-}
+fun applyMovementAndGetTailPositions(rope: Rope, moves: List<Pair<Char, Int>>): Set<Point> =
+    moves.flatMap { m -> (0 until m.second).map { m.first } }
+        .fold(rope to setOf(rope.tail)) { acc, d ->
+            acc.first.moveRopeInDirection(d).let { it to acc.second.plus(it.tail) }
+        }.second
 
-data class Rope(val knots: List<Point>) {
+class Rope(val knots: List<Point>) {
 
     val head = knots.first()
     val tail = knots.last()
 
-    fun moveRopeInDirection(direction: Char): Rope {
-        val newHead = when(direction) {
-            'U' -> Point(head.x, head.y-1)
-            'D' -> Point(head.x, head.y+1)
-            'R' -> Point(head.x+1, head.y)
-            'L' -> Point(head.x-1, head.y)
+    fun moveRopeInDirection(direction: Char): Rope =
+        mutableListOf(head.moveInDirection(direction))
+            .apply { knots.drop(1).forEach { k -> this += k.moveToward(this.last()) } }
+            .let { Rope(it) }
+
+    private fun Point.moveInDirection(direction: Char) =
+        when(direction) {
+            'U' -> Point(x, y-1)
+            'D' -> Point(x, y+1)
+            'R' -> Point(x+1, y)
+            'L' -> Point(x-1, y)
             else -> error("invalid movement")
         }
-        return mutableListOf(newHead).apply {
-            knots.drop(1).forEach { k ->
-                this += k.moveToward(this.last())
-            }
-        }.let { Rope(it) }
-    }
 
-    private fun Point.moveToward(target: Point): Point {
-        var newX = this.x
-        var newY = this.y
-        if(!this.isTouching(target)) {
-            if(this.y > target.y) { newY = this.y - 1 }
-            if(this.y < target.y) { newY = this.y + 1 }
-            if(this.x > target.x) { newX = this.x - 1 }
-            if(this.x < target.x) { newX = this.x + 1 }
-        }
-        return Point(newX, newY)
-    }
+    private fun Point.moveToward(target: Point): Point =
+        if(isTouching(target)) this
+        else Point(x - x.compareTo(target.x), y - y.compareTo(target.y))
     
     private fun Point.isTouching(target: Point) =
         target.x in (this.x-1 .. this.x+1) && target.y in (this.y-1 .. this.y+1)
