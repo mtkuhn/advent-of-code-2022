078fb799d13ba45892077552482449becff651c8
mtkuhn <junkpilemk@protonmail.com>
2022-12-14 09:05:08 -0700

    Day 14 - cleanup

--- src/main/kotlin/mkuhn/aoc/Day14.kt
+++ src/main/kotlin/mkuhn/aoc/Day14.kt
@@ -11,40 +11,37 @@ fun main() {
 
 fun day14part1(input: List<String>): Int {
     val sandOrigin = Point(500, 0)
-    val rock = input.flatMap { line -> line.parseToPoints().segmentsToPoints() }.toMutableSet()
-    return rock.fillSandUntil(sandOrigin) { it == null }.size
+    val rockPoints = input.parseInputToRockPoints()
+    return rockPoints.fillSandUntil(sandOrigin) { it == null }.size
 }
 
 fun day14part2(input: List<String>): Int {
     val sandOrigin = Point(500, 0)
-    val rock = input.flatMap { line -> line.parseToPoints().segmentsToPoints() }.toMutableSet()
+    val rockPoints = input.parseInputToRockPoints()
 
-    val floorY = rock.maxOf { it.y }+2
-    rock += listOf(Point(-5000, floorY), Point(5000, floorY)).segmentsToPoints()
+    val floorY = rockPoints.maxOf { it.y }+2
+    rockPoints += (Point(-5000, floorY) to Point(5000, floorY)).segmentToPoints()
 
-    return rock.fillSandUntil(sandOrigin) { it == sandOrigin }.size
+    return rockPoints.fillSandUntil(sandOrigin) { it == sandOrigin }.size+1
 }
 
 fun Set<Point>.fillSandUntil(sandOrigin: Point, goal: (Point?) -> Boolean): List<Point> {
     val obstructionHeightMap = mutableMapOf<Int, MutableList<Int>>()
     this.forEach { r -> obstructionHeightMap.addPoint(r) }
-    val sand = mutableListOf<Point>()
-
-    var newSand = obstructionHeightMap.findSandRestingPoint(sandOrigin)
-    while(!goal(newSand)) {
-        if(newSand == null) error("something went wrong")
-        obstructionHeightMap.addPoint(newSand)
-        sand += newSand
-        newSand = obstructionHeightMap.findSandRestingPoint(sandOrigin)
-    }
-    if(newSand != null) sand += newSand
-
-    return sand
+    return generateSequence { sandOrigin }
+        .map { obstructionHeightMap.dropSandOrNull(it) }
+        .takeWhile { !goal(it) }
+        .filterNotNull()
+        .toList()
 }
 
-fun String.parseToPoints() = split("""\D+""".toRegex()).chunked(2).map { Point(it[0].toInt(), it[1].toInt()) }
+fun List<String>.parseInputToRockPoints() =
+    flatMap { line -> line.parseLineToPoints()
+        .zipWithNext()
+        .flatMap { it.segmentToPoints() }
+    }.toMutableSet()
 
-fun List<Point>.segmentsToPoints(): Set<Point> = this.zipWithNext().flatMap { it.segmentToPoints() }.toSet()
+fun String.parseLineToPoints() = split("""\D+""".toRegex()).chunked(2).map { Point(it[0].toInt(), it[1].toInt()) }
 
 fun Pair<Point, Point>.segmentToPoints(): Set<Point> =
     (first.x.progressBetween(second.x)).flatMap { xx ->
@@ -59,16 +56,20 @@ fun MutableMap<Int, MutableList<Int>>.addPoint(p: Point) {
     if(this[p.x] == null) { this[p.x] = mutableListOf(p.y) }
     else { this[p.x]?.add(p.y) }
 }
+fun MutableMap<Int, MutableList<Int>>.dropSandOrNull(sandOrigin: Point): Point? {
+    val newSand = findSandRestingPoint(sandOrigin)
+    if(newSand != null) addPoint(newSand)
+    return newSand
+}
 
 fun MutableMap<Int, MutableList<Int>>.findSandRestingPoint(sandOrigin: Point): Point? {
     val yIntersect = this[sandOrigin.x]?.filter { it > sandOrigin.y }?.minOrNull()
-    return if(yIntersect == null) null //the abyss
-    else {
-        val leftDiagBlocked = this[sandOrigin.x-1]?.any { it == yIntersect }?:false
-        val rightDiagBlocked = this[sandOrigin.x+1]?.any { it == yIntersect }?:false
-
-        if(!leftDiagBlocked) findSandRestingPoint(Point(sandOrigin.x-1, yIntersect))
-        else if(!rightDiagBlocked) findSandRestingPoint(Point(sandOrigin.x+1, yIntersect))
-        else Point(sandOrigin.x, yIntersect-1)
+    return when {
+        yIntersect == null -> null
+        isOpen(Point(sandOrigin.x-1, yIntersect)) -> findSandRestingPoint(Point(sandOrigin.x-1, yIntersect))
+        isOpen(Point(sandOrigin.x+1, yIntersect)) -> findSandRestingPoint(Point(sandOrigin.x+1, yIntersect))
+        else -> Point(sandOrigin.x, yIntersect-1)
     }
 }
+
+fun MutableMap<Int, MutableList<Int>>.isOpen(p: Point) = this[p.x]?.none { it == p.y }?:true
