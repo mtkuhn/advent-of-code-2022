849397b6e868ef0c3ce6c0a1734e97cc4bf0da79
mtkuhn <junkpilemk@protonmail.com>
2022-12-16 14:34:35 -0700

    Day 16 - runnable part 1

--- src/main/kotlin/mkuhn/aoc/Day16.kt
+++ src/main/kotlin/mkuhn/aoc/Day16.kt
@@ -1,37 +1,42 @@
 package mkuhn.aoc
 
 import mkuhn.aoc.util.readInput
-import kotlin.math.min
+
+const val MINS = 30
 
 fun main() {
     val input = readInput("Day16")
     println(day16part1(input))
     println(day16part2(input))
-
-    //11 | num: 75801 | best: 1356
 }
 
 fun day16part1(input: List<String>): Int {
     val valves = input.map { Valve.fromString(it) }
-    val initOpenState = valves.associateWith { false }
-    var valvePaths = listOf(ValvePath(valves.first(), initOpenState))
 
-    //todo: create distance map
+    valves.filter { it.flowRate > 0 }.forEach { println(it) }
+
+    //cache of distance values between all nodes
     val distMap = valves.flatMap { v1 -> valves.map { v2 -> v1 to v2 } }
-        //.filter { p -> p.first.name.compareTo(p.second.name) < -1 } } //alphbetical order only
         .associateWith { valves.distanceBetween(it.first, it.second) }
 
-    (0..30).forEach { i ->
-        valvePaths = valvePaths
-            .flatMap { it.expandChoices() }
-            .groupBy { it.curr to it.valvesOpen }.map { m -> m.value.maxBy { it.pressureRelieved } } //an easy performance improvement is to kill off nodes that are objectively worse
+    val init = valves.find { it.name == "AA" }!!
 
-        val best = valvePaths.maxBy { it.pressureRelieved }
-
-        valvePaths = valvePaths.filter { it.maxReliefStillPossible(distMap) >= best.pressureRelieved } //attempt to remove those that can never catch up
-
-        println("$i | num: ${valvePaths.size} | best: ${best.pressureRelieved}")
+    var valvePaths = listOf(ValvePath(
+        init,
+        valves.filter { it.flowRate == 0 } + init, //the 0 flow valves may as well be closed, and the stuck AA valve
+        0,
+        0,
+        emptyList()))
+    var i = 0 //for debugging
+    while(valvePaths.any { it.minsUsed < MINS }) {
+        i++
+        valvePaths = valvePaths.flatMap { v ->
+            v.viableMoves(distMap)
+        }
+        //println("$i | pathnum: ${valvePaths.size} | ${valvePaths.maxOf { it.pressureRelieved }}")
     }
+
+    println(valvePaths.maxBy { it.pressureRelieved }.let { it.history + it.curr.name })
 
     return valvePaths.maxOf { it.pressureRelieved }
 }
@@ -40,45 +45,47 @@ fun day16part2(input: List<String>): Int =
     2
 
 data class ValvePath(val curr: Valve,
-                     val valvesOpen: Map<Valve, Boolean>,
-                     val minsUsed: Int = 0,
-                     val pressureRelieved: Int = 0) {
+                     val closedValves: List<Valve>,
+                     val minsUsed: Int,
+                     val pressureRelieved: Int,
+                     val history: List<String>) {
 
-    fun expandChoices(): List<ValvePath> {
-        val movements = curr.paths.map { p ->
-            valvesOpen.entries
-                .first { m -> m.key.name == p }.key
-        }.map { v -> moveTo(v)}.toMutableList()
+    //move to and open the valve that presents the highest reward (factoring in cost to reach it)
+    //return multiple in the event of a tie
+    fun viableMoves(distMap:  Map<Pair<Valve, Valve>, Int>): List<ValvePath> =
+        if(this.minsUsed == MINS) listOf(this)
+        else {
+            var moves = distMap.filter { it.key.first == curr }
+                .filter { it.key.second !in closedValves }
+                .map { distMapEntry -> this.moveTo(distMapEntry.key.second, distMapEntry.value) }
+                .filter { it.minsUsed <= 30 }
+                .filter { it.pressureRelieved > this.pressureRelieved }
 
-        if(!valvesOpen[curr]!!) { movements += openThisValve() }
-        return movements
+            //todo: how else can we filter this?
+            //is the distance to the furthest node (with a flow) further than remaining minutes?
+
+            moves.ifEmpty {
+                //this means there's nothing else helpful to move to, just use up the minutes
+                listOf(this.copy(minsUsed = MINS))
+            }
+        }
+
+    fun moveTo(newValve: Valve, distance: Int): ValvePath {
+        val minsUsed = minsUsed+distance+1
+        val newPressure = pressureRelieved + (newValve.flowRate*(MINS-minsUsed))
+        return ValvePath(
+            newValve,
+            closedValves+newValve,
+            minsUsed,
+            newPressure,
+            this.history + this.curr.name
+        )
     }
-
-    fun moveTo(v: Valve): ValvePath =
-        ValvePath(
-            v,
-            valvesOpen.toMap(),
-            minsUsed+1,
-            pressureRelieved
-        )
-
-    fun openThisValve(): ValvePath =
-        ValvePath(
-            curr,
-            valvesOpen.toMutableMap().apply { this[curr] = true },
-            minsUsed+1,
-            pressureRelieved + (curr.flowRate * (29-minsUsed))
-        )
-
-    fun maxReliefStillPossible(distMap:  Map<Pair<Valve, Valve>, Int>): Int =
-        pressureRelieved + valvesOpen.filter { !it.value }.keys
-            .filter { (30 - minsUsed) > (distMap[this.curr to it] ?: 0) }
-            .sumOf { it.flowRate * (30 - minsUsed - (distMap[this.curr to it]?:0)) }
 }
 
 data class Valve(val name: String, val flowRate: Int, val paths: List<String>) {
     companion object {
-        val valveRegex = """Valve (.+) has flow rate=(\d+); tunnels? leads? to valves? (.+)""".toRegex()
+        private val valveRegex = """Valve (.+) has flow rate=(\d+); tunnels? leads? to valves? (.+)""".toRegex()
 
         fun fromString(s: String): Valve =
             valveRegex.matchEntire(s)?.destructured?.let {
