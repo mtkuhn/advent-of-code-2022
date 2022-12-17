bea844a1d80e87d9273351051ec20f868a7860dc
mtkuhn <junkpilemk@protonmail.com>
2022-12-16 21:18:12 -0700

    Day 16 - working part 2 test

--- src/main/kotlin/mkuhn/aoc/Day16.kt
+++ src/main/kotlin/mkuhn/aoc/Day16.kt
@@ -2,83 +2,101 @@ package mkuhn.aoc
 
 import mkuhn.aoc.util.readInput
 
-const val MINS = 30
 
 fun main() {
     val input = readInput("Day16")
-    println(day16part1(input))
+    //println(day16part1(input))
     println(day16part2(input))
 }
 
+
 fun day16part1(input: List<String>): Int {
     val valves = input.map { Valve.fromString(it) }
-
-    valves.filter { it.flowRate > 0 }.forEach { println(it) }
-
-    //cache of distance values between all nodes
-    val distMap = valves.flatMap { v1 -> valves.map { v2 -> v1 to v2 } }
-        .associateWith { valves.distanceBetween(it.first, it.second) }
+    val mins = 30
 
     val init = valves.find { it.name == "AA" }!!
+    val you = ValveWalker(init, 0)
 
-    var valvePaths = listOf(ValvePath(
-        init,
-        valves.filter { it.flowRate == 0 } + init, //the 0 flow valves may as well be closed, and the stuck AA valve
+    return findOptimalPath(mapOf(0 to you), valves, mins, init).pressureRelieved
+}
+
+fun day16part2(input: List<String>): Int {
+    val valves = input.map { Valve.fromString(it) }
+    val mins = 26
+
+    val init = valves.find { it.name == "AA" }!!
+    val you = ValveWalker(init, 0)
+    val elephant = ValveWalker(init, 0)
+
+    return findOptimalPath(mapOf(0 to you, 1 to elephant), valves, mins, init).pressureRelieved
+}
+
+fun findOptimalPath(workers: Map<Int, ValveWalker>,
+                    valves: List<Valve>,
+                    mins: Int, initialValve: Valve): DoubleValvePath {
+    val distMap = valves.toDistanceMap()
+    var valvePaths = listOf(DoubleValvePath(
+        workers,
+        valves.filter { it.flowRate == 0 } + initialValve, //the 0 flow valves may as well be closed, and the stuck AA valve
         0,
-        0,
-        emptyList()))
-    var i = 0 //for debugging
-    while(valvePaths.any { it.minsUsed < MINS }) {
+        mins))
+
+    var i = 0
+    while(valvePaths.any { vp -> vp.walkers.all { w -> w.value.minsUsed < mins } }) {
         i++
         valvePaths = valvePaths.flatMap { v ->
             v.viableMoves(distMap)
         }
-        //println("$i | pathnum: ${valvePaths.size} | ${valvePaths.maxOf { it.pressureRelieved }}")
+        println("$i | pathnum: ${valvePaths.size} | ${valvePaths.maxOf { it.pressureRelieved }}")
     }
 
-    println(valvePaths.maxBy { it.pressureRelieved }.let { it.history + it.curr.name })
-
-    return valvePaths.maxOf { it.pressureRelieved }
+    return valvePaths.maxBy { it.pressureRelieved }
 }
 
-fun day16part2(input: List<String>): Int =
-    2
+fun List<Valve>.toDistanceMap(): Map<Pair<Valve, Valve>, Int> {
+    return this.flatMap { v1 ->
+        this.map { v2 -> v1 to v2 }
+    }.associateWith { this.distanceBetween(it.first, it.second) }
+}
 
-data class ValvePath(val curr: Valve,
-                     val closedValves: List<Valve>,
-                     val minsUsed: Int,
-                     val pressureRelieved: Int,
-                     val history: List<String>) {
+data class ValveWalker(val currPos: Valve, val minsUsed: Int)
+
+data class DoubleValvePath(val walkers: Map<Int, ValveWalker>,
+                           val closedValves: List<Valve>,
+                           val pressureRelieved: Int,
+                           val minsAllowed: Int) {
 
     //move to and open the valve that presents the highest reward (factoring in cost to reach it)
     //return multiple in the event of a tie
-    fun viableMoves(distMap:  Map<Pair<Valve, Valve>, Int>): List<ValvePath> =
-        if(this.minsUsed == MINS) listOf(this)
+    fun viableMoves(distMap:  Map<Pair<Valve, Valve>, Int>): List<DoubleValvePath> {
+        val wId = walkers.minBy { it.value.minsUsed }.key
+        return if(walkers[wId]!!.minsUsed == minsAllowed) listOf(this)
         else {
-            var moves = distMap.filter { it.key.first == curr }
-                .filter { it.key.second !in closedValves }
-                .map { distMapEntry -> this.moveTo(distMapEntry.key.second, distMapEntry.value) }
-                .filter { it.minsUsed <= 30 }
+            var moves = distMap.filter { distEntry -> distEntry.key.first == walkers[wId]!!.currPos }
+                .filter { distEntry -> distEntry.key.second !in closedValves }
+                .map { distEntry -> moveTo(wId, distEntry.key.second, distEntry.value) }
+                .filter { it.walkers[wId]!!.minsUsed <= minsAllowed }
                 .filter { it.pressureRelieved > this.pressureRelieved }
 
-            //todo: how else can we filter this?
-            //is the distance to the furthest node (with a flow) further than remaining minutes?
-
             moves.ifEmpty {
-                //this means there's nothing else helpful to move to, just use up the minutes
-                listOf(this.copy(minsUsed = MINS))
+                //this means there's nothing else helpful to move to, just set to 30 mins
+                listOf(this.copy(walkers = walkers.map { it.key to it.value.copy(minsUsed = minsAllowed) }.toMap()))
             }
         }
+    }
 
-    fun moveTo(newValve: Valve, distance: Int): ValvePath {
-        val minsUsed = minsUsed+distance+1
-        val newPressure = pressureRelieved + (newValve.flowRate*(MINS-minsUsed))
-        return ValvePath(
-            newValve,
+    fun moveTo(walkerId: Int, newValve: Valve, distance: Int): DoubleValvePath {
+        val mm = walkers[walkerId]!!.minsUsed+distance+1
+        val newPressure = newValve.flowRate*(minsAllowed-mm)
+        val newWalkers = walkers.toMutableMap().apply { this[walkerId] =
+            ValveWalker(newValve, mm)
+        }
+
+        return DoubleValvePath(
+            newWalkers,
             closedValves+newValve,
-            minsUsed,
-            newPressure,
-            this.history + this.curr.name
+            pressureRelieved + newPressure,
+            minsAllowed
         )
     }
 }
--- src/test/kotlin/mkuhn/aoc/Day16Test.kt
+++ src/test/kotlin/mkuhn/aoc/Day16Test.kt
@@ -16,12 +16,12 @@ internal class Day16Test {
     @Test
     fun testPart1r() {
         val testInput = readInput("Day16")
-        println(day16part1(testInput))
+        assertEquals(1792, day16part1(testInput))
     }
 
     @Test
     fun testPart2() {
         val testInput = readTestInput("Day16_test")
-        assertEquals(0, day16part2(testInput))
+        assertEquals(1707, day16part2(testInput))
     }
 }
